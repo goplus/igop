447ddd5e892f94479793acca5c6d10f463908ec4
visualfc <visualfc@gmail.com>
2021-07-21 22:57:47 +0800

    token.SUB

--- ops.go
+++ ops.go
@@ -897,6 +897,20 @@ func unop(instr *ssa.UnOp, x value) value {
 			return -x
 		case complex128:
 			return -x
+		default:
+			v := reflect.ValueOf(x)
+			r := reflect.New(v.Type()).Elem()
+			switch v.Kind() {
+			case reflect.Int, reflect.Int8, reflect.Int16, reflect.Int32, reflect.Int64:
+				r.SetInt(-v.Int())
+			case reflect.Uint, reflect.Uint8, reflect.Uint16, reflect.Uint32, reflect.Uint64, reflect.Uintptr:
+				r.SetUint(-v.Uint())
+			case reflect.Float32, reflect.Float64:
+				r.SetFloat(-v.Float())
+			case reflect.Complex64, reflect.Complex128:
+				r.SetComplex(-v.Complex())
+			}
+			return r.Interface()
 		}
 	case token.MUL:
 		return reflect.ValueOf(x).Elem().Interface()
