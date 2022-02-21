e4f0131cd020006809d9774c1f96894f95aa360c
visualfc <visualfc@gmail.com>
2022-02-21 21:37:31 +0800

    use funcs types check

--- interp.go
+++ interp.go
@@ -56,9 +56,9 @@ import (
 	"sync/atomic"
 	"unsafe"
 
-	"github.com/goplus/gossa/rta"
 	"github.com/goplus/reflectx"
 	"golang.org/x/tools/go/ssa"
+	"golang.org/x/tools/go/ssa/ssautil"
 	"golang.org/x/tools/go/types/typeutil"
 )
 
@@ -215,7 +215,7 @@ func (i *Interp) toType(typ types.Type) reflect.Type {
 		// 	return t
 		// }
 	}
-	//log.Printf("error toType %v %p\n", typ, typ)
+	log.Panicf("error toType %v %p\n", typ, typ)
 	i.typesMutex.Lock()
 	defer i.typesMutex.Unlock()
 	if t, ok := i.types[typ]; ok {
@@ -1259,27 +1259,67 @@ func NewInterp(loader Loader, mainpkg *ssa.Package, mode Mode) (*Interp, error) 
 			}
 		}
 	}
-	var roots []*ssa.Function
-	roots = append(roots, mainpkg.Func("init"), mainpkg.Func("main"))
-	rtares := rta.Analyze(roots, false)
-	for _, t := range rtares.RuntimeTypes.Keys() {
-		if tuple, ok := t.(*types.Tuple); ok {
+	funcs := ssautil.AllFunctions(i.prog)
+	chkType := func(typ types.Type) {
+		if tuple, ok := typ.(*types.Tuple); ok {
 			for n := 0; n < tuple.Len(); n++ {
 				i.preToType(tuple.At(n).Type())
 			}
 		} else {
-			i.preToType(t)
+			i.preToType(typ)
 		}
 	}
-	for _, t := range rtares.AllocTypes.Keys() {
-		if tuple, ok := t.(*types.Tuple); ok {
-			for n := 0; n < tuple.Len(); n++ {
-				i.preToType(tuple.At(n).Type())
+
+	var space [32]*ssa.Value // preallocate space for common case
+	for f := range funcs {
+		for _, alloc := range f.Locals {
+			chkType(alloc.Type())
+		}
+		for _, b := range f.Blocks {
+			for _, instr := range b.Instrs {
+				switch instr := instr.(type) {
+				case *ssa.Alloc:
+					chkType(instr.Type())
+					if !instr.Heap {
+						chkType(deref(instr.Type()))
+					}
+				case *ssa.Next:
+					chkType(instr.Type())
+				default:
+					rands := instr.Operands(space[:0])
+					for _, op := range rands {
+						if *op == nil {
+							continue
+						}
+						chkType((*op).Type())
+					}
+				}
 			}
-		} else {
-			i.preToType(t)
 		}
 	}
+	/*
+		var roots []*ssa.Function
+		roots = append(roots, mainpkg.Func("init"), mainpkg.Func("main"))
+		rtares := rta.Analyze(roots, false)
+		for _, t := range rtares.RuntimeTypes.Keys() {
+			if tuple, ok := t.(*types.Tuple); ok {
+				for n := 0; n < tuple.Len(); n++ {
+					i.preToType(tuple.At(n).Type())
+				}
+			} else {
+				i.preToType(t)
+			}
+		}
+		for _, t := range rtares.AllocTypes.Keys() {
+			if tuple, ok := t.(*types.Tuple); ok {
+				for n := 0; n < tuple.Len(); n++ {
+					i.preToType(tuple.At(n).Type())
+				}
+			} else {
+				i.preToType(t)
+			}
+		}
+	*/
 
 	_, err := i.Run("init")
 	if err != nil {
