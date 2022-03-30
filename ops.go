02cd05cb7598c0770853140aafdb56da18b1962c
visualfc <visualfc@gmail.com>
2022-03-30 16:04:03 +0800

    add execBuiltinByStack

--- ops.go
+++ ops.go
@@ -1717,6 +1717,99 @@ func (inter *Interp) callBuiltinByStack(caller *frame, fn string, ssaArgs []ssa.
 	}
 }
 
+// execBuiltin interprets a call to builtin fn with arguments args,
+// discards its result.
+func (inter *Interp) execBuiltinByStack(caller *frame, fn string, ssaArgs []ssa.Value, ia []int) {
+	switch fn {
+	case "append":
+		panic("discards result of " + fn)
+
+	case "copy": // copy([]T, []T) int or copy([]byte, string) int
+		arg0 := caller.reg(ia[0])
+		arg1 := caller.reg(ia[1])
+		reflect.Copy(reflect.ValueOf(arg0), reflect.ValueOf(arg1))
+
+	case "close": // close(chan T)
+		arg0 := caller.reg(ia[0])
+		reflect.ValueOf(arg0).Close()
+
+	case "delete": // delete(map[K]value, K)
+		arg0 := caller.reg(ia[0])
+		arg1 := caller.reg(ia[1])
+		reflect.ValueOf(arg0).SetMapIndex(reflect.ValueOf(arg1), reflect.Value{})
+
+	case "print", "println": // print(any, ...)
+		ln := fn == "println"
+		var buf bytes.Buffer
+		for i := 0; i < len(ia); i++ {
+			arg := caller.reg(ia[i])
+			if i > 0 && ln {
+				buf.WriteRune(' ')
+			}
+			if len(ssaArgs) > i {
+				typ := inter.toType(ssaArgs[i].Type())
+				if typ.Kind() == reflect.Interface {
+					buf.WriteString(toInterface(arg))
+					continue
+				}
+			}
+			buf.WriteString(toString(arg))
+		}
+		if ln {
+			buf.WriteRune('\n')
+		}
+		print(buf.Bytes())
+
+	case "len":
+		panic("discards result of " + fn)
+
+	case "cap":
+		panic("discards result of " + fn)
+
+	case "real":
+		panic("discards result of " + fn)
+
+	case "imag":
+		panic("discards result of " + fn)
+
+	case "complex":
+		panic("discards result of " + fn)
+
+	case "panic":
+		// ssa.Panic handles most cases; this is only for "go
+		// panic" or "defer panic".
+		arg0 := caller.reg(ia[0])
+		panic(targetPanic{arg0})
+
+	case "recover":
+		doRecover(caller)
+
+	case "ssa:wrapnilchk":
+		recv := caller.reg(ia[0])
+		if reflect.ValueOf(recv).IsNil() {
+			recvType := caller.reg(ia[1])
+			methodName := caller.reg(ia[2])
+			var info value
+			if s, ok := recvType.(string); ok && strings.HasPrefix(s, "main.") {
+				info = s[5:]
+			} else {
+				info = recvType
+			}
+			panic(plainError(fmt.Sprintf("value method %s.%s called using nil *%s pointer",
+				recvType, methodName, info)))
+		}
+
+	case "Add":
+		panic("discards result of " + fn)
+
+	case "Slice":
+		panic("discards result of " + fn)
+
+	default:
+		panic("unknown built-in: " + fn)
+	}
+}
+
 func rangeIter(x value, t types.Type) iter {
 	switch x := x.(type) {
 	case string:
