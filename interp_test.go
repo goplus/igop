277cb0909194cd969c3e01ea63b5bb6b05e56726
visualfc <visualfc@gmail.com>
2022-04-09 10:35:42 +0800

    add TestBuiltinPrintln

--- interp_test.go
+++ interp_test.go
@@ -16,6 +16,7 @@ package gossa_test
 // fmt or testing, as it proved too fragile.
 
 import (
+	"bytes"
 	"fmt"
 	"log"
 	"os"
@@ -448,5 +449,108 @@ func main() {
 	_, err := gossa.RunFile("main.go", src, nil, 0)
 	if err != nil {
 		t.Fatal(err)
+	}
+}
+
+func TestBuiltinPrintln(t *testing.T) {
+	src := `// run
+
+// Copyright 2014 The Go Authors. All rights reserved.
+// Use of this source code is governed by a BSD-style
+// license that can be found in the LICENSE file.
+
+// Test internal print routines that are generated
+// by the print builtin.  This test is not exhaustive,
+// we're just checking that the formatting is correct.
+
+package main
+
+func main() {
+	println((interface{})(nil)) // printeface
+	println((interface {        // printiface
+		f()
+	})(nil))
+	println((map[int]int)(nil)) // printpointer
+	println(([]int)(nil))       // printslice
+	println(int64(-7))          // printint
+	println(uint64(7))          // printuint
+	println(uint32(7))          // printuint
+	println(uint16(7))          // printuint
+	println(uint8(7))           // printuint
+	println(uint(7))            // printuint
+	println(uintptr(7))         // printuint
+	println(8.0)                // printfloat
+	println(complex(9.0, 10.0)) // printcomplex
+	println(true)               // printbool
+	println(false)              // printbool
+	println("hello")            // printstring
+	println("one", "two")       // printsp
+
+	// test goprintf
+	defer println((interface{})(nil))
+	defer println((interface {
+		f()
+	})(nil))
+	defer println((map[int]int)(nil))
+	defer println(([]int)(nil))
+	defer println(int64(-11))
+	defer println(uint64(12))
+	defer println(uint32(12))
+	defer println(uint16(12))
+	defer println(uint8(12))
+	defer println(uint(12))
+	defer println(uintptr(12))
+	defer println(13.0)
+	defer println(complex(14.0, 15.0))
+	defer println(true)
+	defer println(false)
+	defer println("hello")
+	defer println("one", "two")
+}
+`
+	out := `(0x0,0x0)
+(0x0,0x0)
+0x0
+[0/0]0x0
+-7
+7
+7
+7
+7
+7
+7
++8.000000e+000
+(+9.000000e+000+1.000000e+001i)
+true
+false
+hello
+one two
+one two
+hello
+false
+true
+(+1.400000e+001+1.500000e+001i)
++1.300000e+001
+12
+12
+12
+12
+12
+12
+-11
+[0/0]0x0
+0x0
+(0x0,0x0)
+(0x0,0x0)
+`
+	ctx := gossa.NewContext(0)
+	var buf bytes.Buffer
+	ctx.SetPrintOutput(&buf)
+	_, err := ctx.RunFile("main.go", src, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+	if buf.String() != out {
+		t.Fatal("error")
 	}
 }
