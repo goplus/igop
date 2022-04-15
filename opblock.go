5454ac94de4d17758091dd6a9bf9bfa32b740730
visualfc <visualfc@gmail.com>
2022-04-15 21:57:50 +0800

    makeCallInstr: simplify dynamic func call

--- opblock.go
+++ opblock.go
@@ -1085,24 +1085,14 @@ func makeCallInstr(pfn *Function, interp *Interp, instr ssa.Value, call *ssa.Cal
 	if typ.Kind() != reflect.Func {
 		panic("unsupport")
 	}
-	ia = append(ia, ib...)
 	return func(fr *frame) {
 		fn := fr.reg(iv)
-		switch fn := fn.(type) {
-		case *ssa.Function:
-			interp.callFunctionByStack(fr, interp.funcs[fn], ir, ia)
-		case *closure:
-			interp.callFunctionByStack(fr, fn.pfn, ir, ia)
-		case *ssa.Builtin:
-			interp.callBuiltinByStack(fr, fn.Name(), call.Args, ir, ia)
-		default:
-			if fv, n := funcval.Get(fn); n == 1 {
-				c := (*makeFuncVal)(unsafe.Pointer(fv))
-				interp.callFunctionByStackWithEnv(fr, c.pfn, ir, ia, c.env)
-			} else {
-				v := reflect.ValueOf(fn)
-				interp.callExternalByStack(fr, v, ir, ia)
-			}
+		if fv, n := funcval.Get(fn); n == 1 {
+			c := (*makeFuncVal)(unsafe.Pointer(fv))
+			interp.callFunctionByStackWithEnv(fr, c.pfn, ir, ia, c.env)
+		} else {
+			v := reflect.ValueOf(fn)
+			interp.callExternalByStack(fr, v, ir, ia)
 		}
 	}
 }
