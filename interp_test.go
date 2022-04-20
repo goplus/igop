5301b35f5e45778e2f53c2fae05860d32daaa7d8
visualfc <visualfc@gmail.com>
2022-04-21 07:43:43 +0800

    panic: fix writeany check type error

--- interp_test.go
+++ interp_test.go
@@ -571,6 +571,22 @@ func TestPanicInfo(t *testing.T) {
 	}
 }
 
+func TestPanicError(t *testing.T) {
+	src := `package main
+import "errors"
+func main() {
+	panic(errors.New("error info"))
+}
+`
+	_, err := gossa.RunFile("main.go", src, nil, 0)
+	if err == nil {
+		t.Fatal("must panic")
+	}
+	if s := err.Error(); s != "error info" {
+		t.Fatalf("error %q", s)
+	}
+}
+
 func TestEnablePrintAny(t *testing.T) {
 	src := `package main
 
