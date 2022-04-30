4e54e15aaf216c52f8735f0ab997f7477d4c4bfe
visualfc <visualfc@gmail.com>
2022-04-30 11:09:12 +0800

    makeTypeChangeInstr: ptr chan map func slice use basic.ConvertPtr

--- opconv.go
+++ opconv.go
@@ -22,192 +22,191 @@ func makeTypeChangeInstr(pfn *function, instr *ssa.ChangeType) func(fr *frame) {
 			fr.setReg(ir, v)
 		}
 	}
-	xtyp := pfn.Interp.preToType(instr.X.Type())
+	kind := typ.Kind()
+	switch kind {
+	case reflect.Ptr, reflect.Chan, reflect.Map, reflect.Func, reflect.Slice:
+		t := basic.TypeOfType(typ)
+		return func(fr *frame) {
+			x := fr.reg(ix)
+			fr.setReg(ir, basic.ConvertPtr(t, x))
+		}
+	}
 	isBasic := typ.PkgPath() == ""
-	if kind := typ.Kind(); kind == xtyp.Kind() {
-		if kind == reflect.Ptr {
-			t := basic.TypeOfType(typ)
+	if isBasic {
+		switch kind {
+		case reflect.Bool:
 			return func(fr *frame) {
 				x := fr.reg(ix)
-				fr.setReg(ir, basic.ConvertPtr(t, x))
+				fr.setReg(ir, basic.Bool(x))
+			}
+		case reflect.Int:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.Int(x))
+			}
+		case reflect.Int8:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.Int8(x))
+			}
+		case reflect.Int16:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.Int16(x))
+			}
+		case reflect.Int32:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.Int32(x))
+			}
+		case reflect.Int64:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.Int64(x))
+			}
+		case reflect.Uint:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.Uint(x))
+			}
+		case reflect.Uint8:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.Uint8(x))
+			}
+		case reflect.Uint16:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.Uint16(x))
+			}
+		case reflect.Uint32:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.Uint32(x))
+			}
+		case reflect.Uint64:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.Uint64(x))
+			}
+		case reflect.Uintptr:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.Uintptr(x))
+			}
+		case reflect.Float32:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.Float32(x))
+			}
+		case reflect.Float64:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.Float64(x))
+			}
+		case reflect.Complex64:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.Complex64(x))
+			}
+		case reflect.Complex128:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.Complex128(x))
+			}
+		case reflect.String:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.String(x))
 			}
 		}
-		if isBasic {
-			switch kind {
-			case reflect.Bool:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.Bool(x))
-				}
-			case reflect.Int:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.Int(x))
-				}
-			case reflect.Int8:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.Int8(x))
-				}
-			case reflect.Int16:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.Int16(x))
-				}
-			case reflect.Int32:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.Int32(x))
-				}
-			case reflect.Int64:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.Int64(x))
-				}
-			case reflect.Uint:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.Uint(x))
-				}
-			case reflect.Uint8:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.Uint8(x))
-				}
-			case reflect.Uint16:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.Uint16(x))
-				}
-			case reflect.Uint32:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.Uint32(x))
-				}
-			case reflect.Uint64:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.Uint64(x))
-				}
-			case reflect.Uintptr:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.Uintptr(x))
-				}
-			case reflect.Float32:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.Float32(x))
-				}
-			case reflect.Float64:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.Float64(x))
-				}
-			case reflect.Complex64:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.Complex64(x))
-				}
-			case reflect.Complex128:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.Complex128(x))
-				}
-			case reflect.String:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.String(x))
-				}
+	} else {
+		t := basic.TypeOfType(typ)
+		switch kind {
+		case reflect.Bool:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertBool(t, x))
 			}
-		} else {
-			t := basic.TypeOfType(typ)
-			switch kind {
-			case reflect.Bool:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertBool(t, x))
-				}
-			case reflect.Int:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertInt(t, x))
-				}
-			case reflect.Int8:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertInt8(t, x))
-				}
-			case reflect.Int16:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertInt16(t, x))
-				}
-			case reflect.Int32:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertInt32(t, x))
-				}
-			case reflect.Int64:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertInt64(t, x))
-				}
-			case reflect.Uint:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertUint(t, x))
-				}
-			case reflect.Uint8:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertUint8(t, x))
-				}
-			case reflect.Uint16:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertUint16(t, x))
-				}
-			case reflect.Uint32:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertUint32(t, x))
-				}
-			case reflect.Uint64:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertUint64(t, x))
-				}
-			case reflect.Uintptr:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertUintptr(t, x))
-				}
-			case reflect.Float32:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertFloat32(t, x))
-				}
-			case reflect.Float64:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertFloat64(t, x))
-				}
-			case reflect.Complex64:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertComplex64(t, x))
-				}
-			case reflect.Complex128:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertComplex128(t, x))
-				}
-			case reflect.String:
-				return func(fr *frame) {
-					x := fr.reg(ix)
-					fr.setReg(ir, basic.ConvertString(t, x))
-				}
+		case reflect.Int:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertInt(t, x))
+			}
+		case reflect.Int8:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertInt8(t, x))
+			}
+		case reflect.Int16:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertInt16(t, x))
+			}
+		case reflect.Int32:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertInt32(t, x))
+			}
+		case reflect.Int64:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertInt64(t, x))
+			}
+		case reflect.Uint:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertUint(t, x))
+			}
+		case reflect.Uint8:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertUint8(t, x))
+			}
+		case reflect.Uint16:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertUint16(t, x))
+			}
+		case reflect.Uint32:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertUint32(t, x))
+			}
+		case reflect.Uint64:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertUint64(t, x))
+			}
+		case reflect.Uintptr:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertUintptr(t, x))
+			}
+		case reflect.Float32:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertFloat32(t, x))
+			}
+		case reflect.Float64:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertFloat64(t, x))
+			}
+		case reflect.Complex64:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertComplex64(t, x))
+			}
+		case reflect.Complex128:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertComplex128(t, x))
+			}
+		case reflect.String:
+			return func(fr *frame) {
+				x := fr.reg(ix)
+				fr.setReg(ir, basic.ConvertString(t, x))
 			}
 		}
 	}
