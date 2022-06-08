f5686043219bd3ebc7ac60a5afe0579506f1607b
visualfc <visualfc@gmail.com>
2022-06-08 22:18:57 +0800

    repl: add version

--- cmd/internal/repl/gop.go
+++ cmd/internal/repl/gop.go
@@ -4,9 +4,13 @@
 package repl
 
 import (
+	"fmt"
+
+	"github.com/goplus/gop/env"
 	_ "github.com/goplus/igop/gopbuild"
 )
 
 func init() {
 	supportGoplus = true
+	welcomeGop = fmt.Sprintf("Welcome to Go+ (version %v) REPL!", env.Version())
 }
