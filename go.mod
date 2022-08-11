e90cfbfd7669ee7b1f236305abd73f45c846436c
visualfc <visualfc@gmail.com>
2022-08-11 22:20:30 +0800

    impl GetModulePath

--- go.mod
+++ go.mod
@@ -6,12 +6,12 @@ require (
 	github.com/goplus/gop v1.1.3
 	github.com/goplus/gox v1.11.21
 	github.com/goplus/ipkg v0.1.3
-	github.com/goplus/mod v0.9.12
 	github.com/goplus/reflectx v0.9.5
 	github.com/peterh/liner v1.2.2
 	github.com/petermattis/goid v0.0.0-20220331194723-8ee3e6ded87a
 	github.com/visualfc/funcval v0.1.3
 	github.com/visualfc/xtype v0.1.0
+	golang.org/x/mod v0.6.0-dev.0.20220419223038-86c51ed26bb4
 	golang.org/x/text v0.3.7
 	golang.org/x/tools v0.1.12
 )
--- load/mod.go
+++ load/mod.go
@@ -1,14 +1,62 @@
 package load
 
 import (
-	"github.com/goplus/mod"
-	"github.com/goplus/mod/modload"
+	"errors"
+	"fmt"
+	"io/ioutil"
+	"os"
+	"path/filepath"
+	"strings"
+
+	"golang.org/x/mod/modfile"
 )
 
 func GetModulePath(dir string) (string, error) {
-	m, err := modload.Load(dir, mod.GoModOnly)
+	mod, err := GOMOD(dir)
 	if err != nil {
 		return "", err
 	}
-	return m.Path(), nil
+	f, err := LoadModFile(mod)
+	if err != nil {
+		return "", err
+	}
+	if f.Module != nil {
+		return f.Module.Mod.Path, nil
+	}
+	return "", errors.New("not found mod path")
+}
+
+func GOMOD(dir string) (file string, err error) {
+	if dir == "" {
+		dir = "."
+	}
+	if dir, err = filepath.Abs(dir); err != nil {
+		return
+	}
+	for dir != "" {
+		file = filepath.Join(dir, "go.mod")
+		if fi, e := os.Lstat(file); e == nil && !fi.IsDir() {
+			return
+		}
+		if dir, file = filepath.Split(strings.TrimRight(dir, "/\\")); file == "" {
+			break
+		}
+	}
+	return "", errors.New("not found go.mod")
+}
+
+func LoadModFile(file string) (*modfile.File, error) {
+	data, err := ioutil.ReadFile(file)
+	if err != nil {
+		return nil, err
+	}
+	fix := func(path, vers string) (resolved string, err error) {
+		// do nothing
+		return vers, nil
+	}
+	f, err := modfile.Parse(file, data, fix)
+	if err != nil {
+		return nil, fmt.Errorf("parse go.mod error %w", err)
+	}
+	return f, nil
 }
