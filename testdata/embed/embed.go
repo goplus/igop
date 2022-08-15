86cf27d9ea815957cce99d3313723a961f17def7
visualfc <visualfc@gmail.com>
2022-08-15 08:53:15 +0800

    add TestEmbed

---
+++ testdata/embed/embed.go
@@ -0,0 +1,30 @@
+package main
+
+import (
+	"embed"
+)
+
+//go:embed testdata/data1.txt
+var data1 string
+
+//go:embed testdata/data2.txt
+var data2 []byte
+
+//go:embed testdata/*
+var fs embed.FS
+
+func main() {
+	if data1 != "hello data1" {
+		panic(data1)
+	}
+	if string(data2) != "hello data2" {
+		panic(data2)
+	}
+	data, err := fs.ReadFile("testdata/sub/data2.txt")
+	if err != nil {
+		panic(err)
+	}
+	if string(data) != "sub data2" {
+		panic(data)
+	}
+}
