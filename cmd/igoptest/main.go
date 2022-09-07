5f73d4e95f42fd5c143accf3af6f30e7922aa948
visualfc <visualfc@gmail.com>
2022-09-07 21:42:41 +0800

    igoptest: fix typeparam cons.go issue49547.go

--- cmd/igoptest/main.go
+++ cmd/igoptest/main.go
@@ -75,10 +75,10 @@ func init() {
 		// gorootTestSkips["fixedbugs/issue53635.go"] = "type param"
 
 		gorootTestSkips["typeparam/chans.go"] = "_Ranger Send should have failed, but timed out"
-		gorootTestSkips["typeparam/cons.go"] = "runtime error: interface conversion: main.any is main.Nil, not main.List"
+		// gorootTestSkips["typeparam/cons.go"] = "runtime error: interface conversion: main.any is main.Nil, not main.List"
 		gorootTestSkips["typeparam/issue376214.go"] = "variadic parameter must be of unnamed slice type"
 		gorootTestSkips["typeparam/issue47716.go"] = "got 8, want 8"
-		gorootTestSkips["typeparam/issue49547.go"] = "want: main.F[main.foo], got: main.F"
+		// gorootTestSkips["typeparam/issue49547.go"] = "want: main.F[main.foo], got: main.F"
 		gorootTestSkips["typeparam/nested.go"] = "FAIL"
 
 	case "go1.16":
