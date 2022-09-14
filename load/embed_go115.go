1ce62983217635d69878999ad278e4f9ba11bdfc
visualfc <visualfc@gmail.com>
2022-09-14 12:55:09 +0800

    x

--- load/embed_go115.go
+++ load/embed_go115.go
@@ -10,7 +10,7 @@ import (
 )
 
 func Embed(bp *build.Package, fset *token.FileSet, files []*ast.File, test bool, xtest bool) (*ast.File, error) {
-	return *ast.File, nil
+	return nil, nil
 }
 
 func EmbedFiles(pkgName string, dir string, fset *token.FileSet, files []*ast.File) (*ast.File, error) {
