151f35349563c9e8567439480b38d2c1e0c3c816
visualfc <visualfc@gmail.com>
2023-01-30 20:07:26 +0800

    igoptest: update nested.go info

--- cmd/igoptest/main.go
+++ cmd/igoptest/main.go
@@ -71,7 +71,9 @@ func init() {
 
 		gorootTestSkips["typeparam/chans.go"] = "runtime.SetFinalizer"
 		// gorootTestSkips["typeparam/issue376214.go"] = "build SSA package error: variadic parameter must be of unnamed slice type"
-		gorootTestSkips["typeparam/nested.go"] = "FAIL"
+		if ver != "go1.20" {
+			gorootTestSkips["typeparam/nested.go"] = "skip, run pass but output same as go1.20"
+		}
 		//go1.20
 		gorootTestSkips["fixedbugs/bug514.go"] = "skip cgo"
 		gorootTestSkips["fixedbugs/issue40954.go"] = "skip cgo"
