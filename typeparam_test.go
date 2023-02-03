ba3e495611eea7db5e5eefb667aa914411f6894a
visualfc <visualfc@gmail.com>
2023-02-03 22:06:28 +0800

    TestTypeParamsRecursive

--- typeparam_test.go
+++ typeparam_test.go
@@ -215,3 +215,49 @@ func main() {
 		t.Fatal(err)
 	}
 }
+
+func TestTypeParamsRecursive(t *testing.T) {
+	src := `package main
+
+import "fmt"
+
+func main() {
+	recursive()
+}	
+
+func recursive() {
+	type T int
+	if got, want := recur1[T](5), T(110); got != want {
+		panic(fmt.Sprintf("recur1[T](5) = %d, want = %d", got, want))
+	}
+}
+
+type Integer interface {
+	~int | ~int32 | ~int64
+}
+
+func recur1[T Integer](n T) T {
+	if n == 0 || n == 1 {
+		return T(1)
+	} else {
+		return n * recur2(n-1)
+	}
+}
+
+func recur2[T Integer](n T) T {
+	list := make([]T, n)
+	for i, _ := range list {
+		list[i] = T(i + 1)
+	}
+	var sum T
+	for _, elt := range list {
+		sum += elt
+	}
+	return sum + recur1(n-1)
+}
+`
+	_, err := igop.RunFile("main.go", src, nil, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
