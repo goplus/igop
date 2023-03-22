b678479578a9ea43a412721791fa1865af819e87
visualfc <visualfc@gmail.com>
2023-03-22 17:52:17 +0800

    test: move runtime test

--- runtime_test.go
+++ runtime_test.go
@@ -7,6 +7,619 @@ import (
 	_ "github.com/goplus/igop/pkg/log"
 )
 
+func TestFuncForPC(t *testing.T) {
+	src := `package main
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+)
+
+func test() {
+	println("hello")
+}
+
+func main() {
+	pc := reflect.ValueOf(test).Pointer()
+	fn := runtime.FuncForPC(pc)
+	if fn == nil {
+		panic("error runtime.FuncForPC")
+	}
+	if fn.Name() != "main.test" {
+		panic("error name: " + fn.Name())
+	}
+	file, line := fn.FileLine(fn.Entry())
+	if file != "main.go" {
+		panic("error file:" + file)
+	}
+	if line != 9 {
+		panic(fmt.Errorf("error line: %v", line))
+	}
+}
+`
+	_, err := igop.RunFile("main.go", src, nil, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRuntimeCaller(t *testing.T) {
+	src := `package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+var t *struct {
+	c chan int
+}
+
+var c chan int
+
+func f() {
+	select {
+	case <-t.c: // THIS IS LINE 18
+		break
+	case <-c:
+		break
+	}
+}
+
+func g() {
+	defer func() {
+		panic("error g1")
+	}()
+	defer func() {
+		panic("error g2")
+	}()
+	f()
+	panic("unreachable")
+}
+
+func main() {
+	defer func() {
+		recover()
+		var list []string
+		for i := 0; ; i++ {
+			pc, file, line, ok := runtime.Caller(i)
+			if !ok {
+				break
+			}
+			fn := runtime.FuncForPC(pc)
+			fnName := fn.Name()
+			if fnName == "runtime.gopanic" {
+				list = append(list, "runtime.gopanic")
+			} else if strings.HasPrefix(fnName, "main.") {
+				_, fname := filepath.Split(file)
+				list = append(list, fmt.Sprintf("%v %v:%v", fnName, fname, line))
+			}
+			fmt.Println(pc, fnName, file, line)
+		}
+		if v := fmt.Sprint(list); v != infos {
+			panic(v)
+		}
+	}()
+	g()
+}
+
+var infos = "[main.main.func1 main.go:41 runtime.gopanic main.g.func1 main.go:27 runtime.gopanic main.g.func2 main.go:30 runtime.gopanic main.f main.go:18 main.g main.go:32 main.main main.go:59]"
+`
+	_, err := igop.RunFile("main.go", src, nil, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRuntimeCallers1(t *testing.T) {
+	src := `package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+var t *struct {
+	c chan int
+}
+
+var c chan int
+
+func f() {
+	select {
+	case <-t.c: // THIS IS LINE 18
+		break
+	case <-c:
+		break
+	}
+}
+
+func g() {
+	// defer func() {
+	// 	panic("error g1")
+	// }()
+	// defer func() {
+	// 	panic("error g2")
+	// }()
+	f()
+	panic("unreachable")
+}
+
+func main() {
+	defer func() {
+		recover()
+		rpc := make([]uintptr, 64)
+		runtime.Callers(0, rpc)
+		fs := runtime.CallersFrames(rpc)
+		var list []string
+		for {
+			f, more := fs.Next()
+			if f.Function == "runtime.gopanic" {
+				list = append(list, "runtime.gopanic")
+			} else if strings.HasPrefix(f.Function, "main.") {
+				_, fname := filepath.Split(f.File)
+				list = append(list, fmt.Sprintf("%v %v:%v", f.Function, fname, f.Line))
+			}
+			fmt.Println(f)
+			if !more {
+				break
+			}
+		}
+		if v := fmt.Sprint(list); v != infos {
+			panic(v)
+		}
+	}()
+	g()
+}
+
+var infos = "[main.main.func1 main.go:40 runtime.gopanic main.f main.go:18 main.g main.go:32 main.main main.go:60]"
+`
+	_, err := igop.RunFile("main.go", src, nil, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRuntimeCallers2(t *testing.T) {
+	src := `package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+var t *struct {
+	c chan int
+}
+
+var c chan int
+
+func f() {
+	select {
+	case <-t.c: // THIS IS LINE 18
+		break
+	case <-c:
+		break
+	}
+}
+
+func g() {
+	defer func() {
+		panic("error g1")
+	}()
+	// defer func() {
+	// 	panic("error g2")
+	// }()
+	f()
+	panic("unreachable")
+}
+
+func main() {
+	defer func() {
+		recover()
+		rpc := make([]uintptr, 64)
+		runtime.Callers(0, rpc)
+		fs := runtime.CallersFrames(rpc)
+		var list []string
+		for {
+			f, more := fs.Next()
+			if f.Function == "runtime.gopanic" {
+				list = append(list, "runtime.gopanic")
+			} else if strings.HasPrefix(f.Function, "main.") {
+				_, fname := filepath.Split(f.File)
+				list = append(list, fmt.Sprintf("%v %v:%v", f.Function, fname, f.Line))
+			}
+			fmt.Println(f)
+			if !more {
+				break
+			}
+		}
+		if v := fmt.Sprint(list); v != infos {
+			panic(v)
+		}
+	}()
+	g()
+}
+
+var infos = "[main.main.func1 main.go:40 runtime.gopanic main.g.func1 main.go:27 runtime.gopanic main.f main.go:18 main.g main.go:32 main.main main.go:60]"
+`
+	_, err := igop.RunFile("main.go", src, nil, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRuntimeCallers3(t *testing.T) {
+	src := `package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+var t *struct {
+	c chan int
+}
+
+var c chan int
+
+func f() {
+	select {
+	case <-t.c: // THIS IS LINE 18
+		break
+	case <-c:
+		break
+	}
+}
+
+func g() {
+	defer func() {
+		panic("error g1")
+	}()
+	defer func() {
+		panic("error g2")
+	}()
+	f()
+	panic("unreachable")
+}
+
+func main() {
+	defer func() {
+		recover()
+		rpc := make([]uintptr, 64)
+		runtime.Callers(0, rpc)
+		fs := runtime.CallersFrames(rpc)
+		var list []string
+		for {
+			f, more := fs.Next()
+			if f.Function == "runtime.gopanic" {
+				list = append(list, "runtime.gopanic")
+			} else if strings.HasPrefix(f.Function, "main.") {
+				_, fname := filepath.Split(f.File)
+				list = append(list, fmt.Sprintf("%v %v:%v", f.Function, fname, f.Line))
+			}
+			fmt.Println(f)
+			if !more {
+				break
+			}
+		}
+		if v := fmt.Sprint(list); v != infos {
+			panic(v)
+		}
+	}()
+	g()
+}
+
+var infos = "[main.main.func1 main.go:40 runtime.gopanic main.g.func1 main.go:27 runtime.gopanic main.g.func2 main.go:30 runtime.gopanic main.f main.go:18 main.g main.go:32 main.main main.go:60]"
+`
+	_, err := igop.RunFile("main.go", src, nil, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRuntimeCallers4(t *testing.T) {
+	src := `package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+var t *struct {
+	c chan int
+}
+
+var c chan int
+
+func f() {
+	select {
+	case <-t.c: // THIS IS LINE 18
+		break
+	case <-c:
+		break
+	}
+}
+
+func g() {
+	defer func() {
+		// panic("error g1")
+	}()
+	defer func() {
+		// panic("error g2")
+		f()
+	}()
+	// f()
+}
+
+func main() {
+	defer func() {
+		recover()
+		rpc := make([]uintptr, 64)
+		runtime.Callers(0, rpc)
+		fs := runtime.CallersFrames(rpc)
+		var list []string
+		for {
+			f, more := fs.Next()
+			if f.Function == "runtime.gopanic" {
+				list = append(list, "runtime.gopanic")
+			} else if strings.HasPrefix(f.Function, "main.") {
+				_, fname := filepath.Split(f.File)
+				list = append(list, fmt.Sprintf("%v %v:%v", f.Function, fname, f.Line))
+			}
+			fmt.Println(f)
+			if !more {
+				break
+			}
+		}
+		if v := fmt.Sprint(list); v != infos {
+			panic(v)
+		}
+	}()
+	g()
+}
+
+var infos = "[main.main.func1 main.go:40 runtime.gopanic main.f main.go:18 main.g.func2 main.go:31 main.g main.go:34 main.main main.go:60]"
+`
+	_, err := igop.RunFile("main.go", src, nil, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRuntimeCallers5(t *testing.T) {
+	src := `package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+var t *struct {
+	c chan int
+}
+
+var c chan int
+
+func f() {
+	select {
+	case <-t.c: // THIS IS LINE 18
+		break
+	case <-c:
+		break
+	}
+}
+
+func g() {
+	defer func() {
+		// panic("error g1")
+	}()
+	defer func() {
+		// panic("error g2")
+		f()
+	}()
+	f()
+}
+
+func main() {
+	defer func() {
+		recover()
+		rpc := make([]uintptr, 64)
+		runtime.Callers(0, rpc)
+		fs := runtime.CallersFrames(rpc)
+		var list []string
+		for {
+			f, more := fs.Next()
+			if f.Function == "runtime.gopanic" {
+				list = append(list, "runtime.gopanic")
+			} else if strings.HasPrefix(f.Function, "main.") {
+				_, fname := filepath.Split(f.File)
+				list = append(list, fmt.Sprintf("%v %v:%v", f.Function, fname, f.Line))
+			}
+			fmt.Println(f)
+			if !more {
+				break
+			}
+		}
+		if v := fmt.Sprint(list); v != infos {
+			panic(v)
+		}
+	}()
+	g()
+}
+
+var infos = "[main.main.func1 main.go:40 runtime.gopanic main.f main.go:18 main.g.func2 main.go:31 runtime.gopanic main.f main.go:18 main.g main.go:33 main.main main.go:60]"
+`
+	_, err := igop.RunFile("main.go", src, nil, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRuntimeCallers6(t *testing.T) {
+	src := `package main
+
+import (
+	"fmt"
+	"path/filepath"
+	"runtime"
+	"strings"
+)
+
+var t *struct {
+	c chan int
+}
+
+var c chan int
+
+func f() {
+	select {
+	case <-t.c: // THIS IS LINE 18
+		break
+	case <-c:
+		break
+	}
+}
+
+func g() {
+	defer func() {
+		panic("error g1")
+	}()
+	defer func() {
+		// panic("error g2")
+		f()
+	}()
+	// f()
+}
+
+func main() {
+	defer func() {
+		recover()
+		rpc := make([]uintptr, 64)
+		runtime.Callers(0, rpc)
+		fs := runtime.CallersFrames(rpc)
+		var list []string
+		for {
+			f, more := fs.Next()
+			if f.Function == "runtime.gopanic" {
+				list = append(list, "runtime.gopanic")
+			} else if strings.HasPrefix(f.Function, "main.") {
+				_, fname := filepath.Split(f.File)
+				list = append(list, fmt.Sprintf("%v %v:%v", f.Function, fname, f.Line))
+			}
+			fmt.Println(f)
+			if !more {
+				break
+			}
+		}
+		if v := fmt.Sprint(list); v != infos {
+			panic(v)
+		}
+	}()
+	g()
+}
+
+var infos = "[main.main.func1 main.go:40 runtime.gopanic main.g.func1 main.go:27 runtime.gopanic main.f main.go:18 main.g.func2 main.go:31 main.g main.go:34 main.main main.go:60]"
+`
+	_, err := igop.RunFile("main.go", src, nil, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
+func TestRuntimeCallerMethodWrapper(t *testing.T) {
+	src := `package main
+
+import (
+	"fmt"
+	"reflect"
+	"runtime"
+	"strings"
+)
+
+type T int
+
+func (T) f() int { return 0 }
+
+func (*T) g() int { return 0 }
+
+type I interface {
+	f() int
+}
+
+var (
+	index int
+)
+
+func check(v interface{}, name string, autogen bool) {
+	index++
+	pc := reflect.ValueOf(v).Pointer()
+	fn := runtime.FuncForPC(pc)
+	typ := reflect.ValueOf(v).Type()
+	file, line := fn.FileLine(pc)
+	fmt.Println(typ, fn.Name(), file, line)
+	if name != "" && name != fn.Name() {
+		panic(fmt.Errorf("%v: name got %v, want %v", index, fn.Name(), name))
+	}
+	if autogen {
+		if file != "<autogenerated>" || line != 1 {
+			panic(fmt.Errorf("%v: file must autogen %v %v", index, file, line))
+		}
+	} else {
+		if !strings.Contains(file, "main.go") {
+			panic(fmt.Errorf("%v: file must main.go %v %v", index, file, line))
+		}
+	}
+}
+
+func main() {
+	ver := runtime.Version()[:6]
+	if ver < "go1.18" {
+		// $thunk
+		check(T.f, "main.T.f", false)
+		check((*T).g, "main.(*T).g", false)
+		check((struct{ T }).f, "", true)
+		check((struct{ *T }).g, "", true)
+		// $bound
+		check(T(0).f, "main.T.f-fm", false)
+		check(new(T).f, "main.T.f-fm", false)
+		check(new(T).g, "main.(*T).g-fm", false)
+		// wrapper
+		check(I.f, "main.I.f", true)
+	} else {
+		// $thunk
+		check(T.f, "main.T.f", false)
+		check((*T).g, "main.(*T).g", false)
+		check((struct{ T }).f, "", true)
+		check((struct{ *T }).g, "", true)
+		// $bound
+		check(T(0).f, "main.T.f-fm", true)
+		check(new(T).f, "main.T.f-fm", true)
+		check(new(T).g, "main.(*T).g-fm", true)
+		// wrapper
+		check(I.f, "main.I.f", true)
+	}
+}
+`
+	_, err := igop.RunFile("main.go", src, nil, 0)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestGC15281(t *testing.T) {
 	// $GOROOT/test/fixedbugs/issue15281.go
 	src := `// run
