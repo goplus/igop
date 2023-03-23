1e2284ae072dcafe462d5ae4a774b3666376e3a5
visualfc <visualfc@gmail.com>
2023-03-23 16:17:40 +0800

    TestLinknameVar

--- runtime_test.go
+++ runtime_test.go
@@ -845,6 +845,49 @@ func main() {
 	}
 }
 
+func TestLinknameVar(t *testing.T) {
+	pkg := `package pkg
+var n int = 100
+func N() int {
+	return n
+}
+`
+	src := `package main
+import (
+	_ "unsafe"
+	"pkg"
+	"fmt"
+)
+
+//go:linkname v pkg.n
+var v int
+
+//go:linkname e igop_test_linkname.e
+var e int
+
+func main() {
+	if v != 100 {
+		panic(fmt.Errorf("v got %v, must 100",v))
+	}
+	v = 200
+	if pkg.N() != 200 {
+		panic(fmt.Errorf("pkg.N got %v, must 200",pkg.N()))
+	}
+	if e != 100 {
+		panic(fmt.Errorf("3 got %v, must 100",e))
+	}
+}
+`
+	ctx := igop.NewContext(0)
+	ctx.AddImportFile("pkg", "pkg.go", pkg)
+	var e int = 100
+	igop.RegisterExternal("igop_test_linkname.e", &e)
+	_, err := ctx.RunFile("main.go", src, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+}
+
 func TestLinknameError1(t *testing.T) {
 	pkg := `package pkg
 func add(v ...int) (sum int) {
