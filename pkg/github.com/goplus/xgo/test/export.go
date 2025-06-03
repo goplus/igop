d73bcbe0fa5df4dd8af9080a0a74b95cb4956029
visualfc <visualfc@gmail.com>
2025-06-03 21:39:16 +0800

    xgobuild/pkg: generate xgo/test remove -code

--- pkg/github.com/goplus/xgo/test/export.go
+++ pkg/github.com/goplus/xgo/test/export.go
@@ -34,8 +34,5 @@ func init() {
 		UntypedConsts: map[string]ixgo.UntypedConst{
 			"GopPackage": {"untyped bool", constant.MakeBool(bool(q.GopPackage))},
 		},
-		Source: source,
 	})
 }
-
-var source = "package test\n\nimport (\n\t\"os\"\n\t\"testing\"\n)\n\nconst (\n\tGopPackage = true\n)\n\ntype testingT = testing.T\n\ntype Case struct {\n\tt *testingT\n}\n\nfunc (p *Case) initCase(t *testing.T) {\n\tp.t = t\n}\n\nfunc (p Case) T() *testing.T\t{ return p.t }\n\nfunc (p Case) Run(name string, f func(t *testing.T)) bool {\n\treturn p.t.Run(name, f)\n}\n\nfunc Gopt_Case_TestMain(c interface{ initCase(t *testing.T) }, t *testing.T) {\n\tc.initCase(t)\n\tc.(interface{ Main() }).Main()\n}\n\ntype App struct {\n\tm *testing.M\n}\n\nfunc (p *App) initApp(m *testing.M) {\n\tp.m = m\n}\n\nfunc (p App) M() *testing.M\t{ return p.m }\n\nfunc Gopt_App_TestMain(app interface{ initApp(m *testing.M) }, m *testing.M) {\n\tapp.initApp(m)\n\tif me, ok := app.(interface{ MainEntry() }); ok {\n\t\tme.MainEntry()\n\t}\n\tos.Exit(m.Run())\n}\n"
--- xgobuild/pkg/pkg.go
+++ xgobuild/pkg/pkg.go
@@ -7,7 +7,7 @@ package pkg
 //go:generate go run ../../cmd/qexp -outdir ../../pkg github.com/goplus/xgo/scanner
 //go:generate go run ../../cmd/qexp -outdir ../../pkg github.com/goplus/xgo/token
 //go:generate go run ../../cmd/qexp -outdir ../../pkg github.com/goplus/xgo/tpl/...
-//go:generate go run ../../cmd/qexp -outdir ../../pkg -code github.com/goplus/xgo/test
+//go:generate go run ../../cmd/qexp -outdir ../../pkg github.com/goplus/xgo/test
 
 import (
 	_ "github.com/goplus/ixgo/xgobuild/pkg/gsh"
